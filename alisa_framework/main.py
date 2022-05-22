50b61bbf793a434beb9b7e7bfd5d12af19c8128c
AlisaP8 <78349377+AlisaP8@users.noreply.github.com>
2022-05-22 16:01:44 +0400

    lesson_1

--- alisa_framework/main.py
+++ alisa_framework/main.py
@@ -1,25 +1,26 @@
 class PageNotFound404:
     def __call__(self, request):
-        return '404 PAGE Not Found'
+        return '404 WHAT', '404 PAGE Not Found'
 
 
 class Framework:
     def __init__(self, routes_obj, fronts_obj):
-        self.routes_obj = routes_obj
-        self.fronts_obj = fronts_obj
+        self.routes_lst = routes_obj
+        self.fronts_lst = fronts_obj
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
+
         if not path.endswith('/'):
             path = f'{path}/'
 
-        if path in self.routes_obj:
-            view = self.routes_obj[path]
+        if path in self.routes_lst:
+            view = self.routes_lst[path]
         else:
             view = PageNotFound404()
         request = {}
 
-        for front in self.fronts_obj:
+        for front in self.fronts_lst:
             front(request)
 
         code, body = view(request)
